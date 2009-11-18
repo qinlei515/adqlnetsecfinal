d7b6e3f67efa68fa51a9a2fe8cc3b6866b744924
acdubre <acdubre@1719890e-d32a-11de-89a3-7d4a0ca7bf7e>
2009-11-18 01:19:11 +0000

    Refactor to attempt branch functionality.

---
+++ acdubre/src/protocol/Common.java
@@ -0,0 +1,67 @@
+package protocol;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import utils.BufferUtils;
+
+public class Common 
+{
+	
+	// Challenge 1: Prove we're here.
+	public static byte[] handleChallenge1(DataInputStream fromServer) throws IOException
+	{
+		return getResponse(fromServer);
+	}
+	
+	// Challenge 2: Guess-the-number
+	public static byte[] handleChallenge2(DataInputStream fromServer) throws NoSuchAlgorithmException, IOException
+	{
+		byte[] resp2 = getResponse(fromServer);
+		byte[] hash = new byte[utils.Constants.C_HASH_SIZE];
+		BufferUtils.copy(resp2, hash, hash.length);
+		int sizeOfR = resp2[hash.length];
+		byte[] number = new byte[sizeOfR];
+		BufferUtils.copy(resp2, number, sizeOfR, hash.length+1);
+		Common.guessTheNumber(hash, number);
+		return number;
+	}
+	
+	public static byte[] getResponse(DataInputStream fromServer) throws IOException
+	{
+		//TODO: Get the size of the response as the first two bytes of the reply, then allocate the response array.
+		int responseSize = fromServer.read() * 256 + fromServer.read();
+		byte[] response = new byte[responseSize];
+		fromServer.read(response);
+		return response;
+	}
+	
+	public static void guessTheNumber(byte[] hash, byte[] given) throws NoSuchAlgorithmException
+	{
+		MessageDigest md = MessageDigest.getInstance(utils.Constants.CHALLENGE_HASH);		
+		md.update(given);
+		byte[] ourHash = md.digest();
+		boolean done = BufferUtils.equals(hash, ourHash);
+		while(!done)
+		{
+			plusOne(given);
+			md.update(given);
+			ourHash = md.digest();
+			done = BufferUtils.equals(hash, ourHash);
+		}
+	}
+	
+	// Interpret number as an integer; add one to it.
+	private static void plusOne(byte[] number)
+	{
+		int i = 0;
+		while(number[i] == Byte.MAX_VALUE)
+		{
+			number[i] = 0;
+			i++;
+		}
+		number[i]++;
+	}
+}
