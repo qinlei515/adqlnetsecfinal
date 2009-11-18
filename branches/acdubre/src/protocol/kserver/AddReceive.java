3ed8cc21816e37e4c212b2e1efa2c174c503bc7f
acdubre <acdubre@1719890e-d32a-11de-89a3-7d4a0ca7bf7e>
2009-11-18 17:30:54 +0000

    Continued client side Trans 2. Added the beginning of server side Trans 2. Added unit tests for BufferUtils.

---
+++ branches/acdubre/src/protocol/kserver/AddReceive.java
@@ -0,0 +1,23 @@
+package protocol.kserver;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import javax.crypto.spec.DHParameterSpec;
+
+public class AddReceive 
+{
+
+	
+	public boolean doReceive(DHParameterSpec ourSpecs, Socket client)
+	{
+		try 
+		{
+			KSCommon.provideChallenge1(client);
+		} 
+		catch (IOException e) {	e.printStackTrace(); }
+		return false;
+	}
+}
