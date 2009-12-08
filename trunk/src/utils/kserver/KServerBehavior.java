721ff26961342870346afa38ff742791101266b3
acdubre <acdubre@1719890e-d32a-11de-89a3-7d4a0ca7bf7e>
2009-12-08 00:44:29 +0000

    Stubbed in all(?) protocols. protocol.client.XRequest.java and protocol.server.X.java should be developed in parallel.
    Servers need work - empty responses from a client will crash them.

--- trunk/src/utils/kserver/KServerBehavior.java
+++ trunk/src/utils/kserver/KServerBehavior.java
@@ -1,17 +1,60 @@
 package utils.kserver;
 
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
 
-import javax.crypto.SecretKey;
+import javax.crypto.Cipher;
 
+import protocol.Protocol;
+import protocol.Requests;
+import protocol.client.Common;
+import protocol.server.BadRequest;
+import protocol.server.KSAdd;
+import protocol.server.KSPrivate;
+import protocol.server.KSPublic;
+import protocol.server.KSUpdate;
+
+import utils.BufferUtils;
 import utils.server.ServerBehavior;
 
 public class KServerBehavior implements ServerBehavior 
 {
-
-	public void handleConnection(SecretKey sessionKey, Socket connection) 
+	
+	public void handleConnection(Cipher sessionCipher, Socket connection) 
 	{
-		// TODO: Implement the actual key server functionality here.
+		try
+		{
+			DataInputStream fromClient = new DataInputStream(connection.getInputStream());
+			
+			ArrayList<byte[]> request = Common.getResponse(fromClient);
+			Protocol p;
+			
+			if(BufferUtils.equals(request.get(0), Requests.ADD))
+			{
+				p = new KSAdd();
+			}
+			else if(BufferUtils.equals(request.get(0), Requests.UPDATE))
+			{
+				p = new KSUpdate();
+			}
+			else if(BufferUtils.equals(request.get(0), Requests.PUBLIC))
+			{
+				p = new KSPublic();
+			}
+			else if(BufferUtils.equals(request.get(0), Requests.PRIVATE))
+			{
+				p = new KSPrivate();
+			}
+			else
+			{
+				p = new BadRequest();
+			}
+			p.run(connection, sessionCipher);
+			
+		}
+		catch(IOException e) { e.printStackTrace(); }
 	}
 
 }
